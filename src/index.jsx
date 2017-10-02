6cb94fd8a264c529d7ec322b628145917ae8833e
stekolschikov <stekolschikov@mail.ua>
2017-10-02 11:28:04 +0300

    sass??

---
+++ src/index.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import {render} from 'react-dom';
+import AwesomeComponent from './AwesomeComponent.jsx';
+// import './style.sass';
+
+class App extends React.Component {
+    render () {
+      return (
+        <div>
+          <p> Hello React Project!!!</p>
+          <AwesomeComponent />
+        </div>
+      );
+    }
+  }
+
+render(<App/>, document.getElementById('app'));
