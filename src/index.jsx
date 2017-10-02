e3e3b76034460099743172ffdf91d89ac943ac09
stekolschikov <stekolschikov@mail.ua>
2017-10-02 12:21:35 +0300

    sass???

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import {render} from 'react-dom';
 import AwesomeComponent from './AwesomeComponent.jsx';
-// import './style.sass';
+import './style.sass';
 
 class App extends React.Component {
     render () {
--- webpack.config.js
+++ webpack.config.js
@@ -21,6 +21,17 @@ var config = {
                 loader: 'babel-loader'
             }
         ]
+        ,
+        rules: [{
+            test: /\.scss$/,
+            use: [{
+                loader: "style-loader" // creates style nodes from JS strings
+            }, {
+                loader: "css-loader" // translates CSS into CommonJS
+            }, {
+                loader: "sass-loader" // compiles Sass to CSS
+            }]
+        }]
     }
 };
 
