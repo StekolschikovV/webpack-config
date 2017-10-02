a4c349f33fadd3424f06ab780bae0d92b7ab7e3f
stekolschikov <stekolschikov@mail.ua>
2017-10-02 10:18:18 +0300

    DEV SERVER

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,10 @@
 var webpack = require('webpack');
 var path = require('path');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
 
-var BUILD_DIR = path.resolve(__dirname, 'src/client/public');
+var BUILD_DIR = path.resolve(__dirname, './');
 var APP_DIR = path.resolve(__dirname, 'src/client/app');
+var INDEX = path.resolve(__dirname, 'src/client/');
 
 var config = {
     entry: APP_DIR + '/index.jsx',
@@ -10,6 +12,38 @@ var config = {
         path: BUILD_DIR,
         filename: 'bundle.js'
     },
+    devServer: {
+        contentBase: BUILD_DIR
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.jsx?$/,
+                exclude: [/node_modules/],
+                loader: "babel-loader",
+                query: {
+                    presets: ['es2015', 'react', 'stage-0', 'stage-1']
+                }
+            },
+            {
+                test: /\.scss$/,
+                loader: ExtractTextPlugin.extract('style-loader', 'css-loader!resolve-url!sass-loader?sourceMap')
+            },
+            {
+                test: /\.css$/,
+                loader: ExtractTextPlugin.extract('style-loader', 'css-loader')
+            },
+            {
+                test: /\.woff2?$|\.ttf$|\.eot$|\.svg$|\.png|\.jpe?g|\.gif$/,
+                loader: 'file-loader'
+            }
+        ]
+    },
+    plugins: [
+        new ExtractTextPlugin('styles.css', {
+            allChunks: true
+        })
+    ],
     module: {
         loaders: [
             {
